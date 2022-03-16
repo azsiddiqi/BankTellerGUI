9fabf47bb1f63fb99bdffe0a0e2e70032b5f59dd
Karan Patel <karan.patel4@rutgers.edu>
2022-03-15 22:29:54 -0400

    added stuff

--- src/main/java/com/example/banktellergui/HelloController.java
+++ src/main/java/com/example/banktellergui/HelloController.java
@@ -1,14 +1,50 @@
 package com.example.banktellergui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ToggleGroup accType;
+
+    @FXML
+    private RadioButton accountType;
+
+    @FXML
+    private ToggleGroup campCode;
+
+    @FXML
+    private RadioButton campusCode;
+
+    @FXML
+    private TextArea displayResult;
+
+    @FXML
+    private DatePicker dob;
+
+    @FXML
+    private TextField fname;
+
+    @FXML
+    private TextField lname;
+
+    @FXML
+    private RadioButton loyalCustomer;
+
+    @FXML
+    void closeAccount(ActionEvent event) {
+
     }
+
+    @FXML
+    void openAccount(ActionEvent event) {
+
+    }
+
 }
